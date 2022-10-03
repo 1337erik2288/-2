cff5b3bb074ef89520eafe871028de42eca36623
Koncheny_FNAFer228 <Koncheny_FNAFer228@LAPTOP-BN9SOC5T>
2022-10-04 00:36:32 +0800

    Добавьте файлы проекта.

---
+++ Лаба2/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Лаба2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numNBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Число копеек - N";
+            // 
+            // numNBox1
+            // 
+            this.numNBox1.Location = new System.Drawing.Point(68, 83);
+            this.numNBox1.Name = "numNBox1";
+            this.numNBox1.Size = new System.Drawing.Size(150, 31);
+            this.numNBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(90, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Батон";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 216);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numNBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox numNBox1;
+        private Button button1;
+    }
+}
---
+++ Лаба2/Form1.cs
@@ -0,0 +1,85 @@
+namespace Лаба2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            numNBox1.Text = Properties.Settings.Default.money.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            double money;
+            try
+            {
+                money = double.Parse(this.numNBox1.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Одна ошибка и ты ошибся🐺", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);   
+                return;
+            }
+            Properties.Settings.Default.money = money;
+            Properties.Settings.Default.Save();
+
+            MessageBox.Show(Logic.Compare(money));
+        }
+
+        public class Logic
+        {
+            public static string Compare(double n)
+            {
+                string outMessage1 = "";
+                double rub = 0, kop = 0;
+                if (n == 0)
+                {
+                    outMessage1 = "Нет денег";
+                }
+                if (n > 0 && n < 10000)
+                {
+                    rub = (int)n / 100;
+                    kop = n % 100;
+                    double help;
+                    help = kop % 10;
+                    if (help == 5 || help == 6 || help == 7 || help == 8 || help == 9 || kop == 10 || kop == 11 || kop == 12 || kop == 13 || kop == 14)
+                    {
+                        outMessage1 = $"{kop} копеек";
+                    }
+                    else
+                    {
+                        if (help == 1)
+                        {
+                            outMessage1 = $"{kop} копейка";
+                        }
+                        else
+                        {
+                            outMessage1 = $"{kop} копейки";
+                        }
+                    }
+                    if (rub > 0)
+                    {
+                        help = rub % 10;
+                        if (help == 5 || help == 6 || help == 7 || help == 8 || help == 9 || rub == 10 || rub == 11 || rub == 12 || rub == 13 || rub == 14)
+                        {
+                            outMessage1 += $" {rub} рублей";
+                        }
+                        else
+                        {
+                            if (help == 1)
+                            {
+                                outMessage1 += $" {rub} рубль";
+                            }
+                            else
+                            {
+                                outMessage1 += $" {rub} рубля";
+                            }
+                        }
+                    }
+                }
+                return outMessage1;
+            }
+        }
+    }
+}
